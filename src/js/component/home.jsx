d455aabfbb7aa37044553d495b4c2eef03c84151
ChiaraR3 <chiara.r.3@live.it>
2021-08-22 13:52:10 +0000

    errore map

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tasks, setTasks] = useState("Task, Task 2");
+	const [newTask, setNewTask] = useState("");
+
+	function NewTaskChange(event) {
+		setNewTask(event.target.value);
+	}
+	function AddNewOne(event) {
+		if (event.key.toLowerCase() === "enter") setTasks([...tasks, newTask]);
+		setNewTask(" ");
+	}
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input
+				type="text"
+				placeholder="New Task"
+				onChange={NewTaskChange}
+				onKeyPress={AddNewOne}
+				value={newTask}
+			/>
+			<ul>
+				{tasks.map((task, index) => (
+					<li key={index}>
+						<span>{task}</span>
+					</li>
+				))}
+				<li> task 1</li>
+				<li> task 2</li>
+				<li> task 3</li>
+			</ul>
 		</div>
 	);
 };
